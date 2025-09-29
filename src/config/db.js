831bf51c61b69575a8b1c36afb5fb4eafa562bb9
Zahidalwan <zahidalwan1601@gmail.com>
2025-09-29 14:07:59 +0700

    first commit

---
+++ src/config/db.js
@@ -0,0 +1,18 @@
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "phone_store",
+});
+
+export const testConection = async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("DB is connected");
+    connection.release();
+  } catch (error) {
+    console.error(error);
+  }
+};
