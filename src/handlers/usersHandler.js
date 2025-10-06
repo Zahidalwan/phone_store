86ed387c9a75367143dd8313efa195a05d4d70c4
Zahidalwan <zahidalwan1601@gmail.com>
2025-10-06 14:25:51 +0700

    user

--- src/handlers/usersHandler.js
+++ src/handlers/usersHandler.js
@@ -27,11 +27,7 @@ export const getUserByIdHandler = async (req, res) => {
   const { id } = req.params;
   try {
     const [users] = await pool.query(
-      `SELECT 
-        id, fullname, username, email, role,
-        address, phone_number, age
-       FROM users
-       WHERE id = ?`,
+      `SELECT id, fullname, username, email, role, address, phone_number, age FROM users WHERE id = ?`,
       [id]
     );
 
@@ -135,5 +131,82 @@ export const addUserHandler = async (req, res) => {
       status: "error",
       message: "Internal server error",
     });
+  }
+};
+
+export const updateUserHandler = async (req, res) => {
+  const { id } = req.params;
+  const {
+    fullname,
+    username,
+    email,
+    password,
+    role,
+    address,
+    phone_number,
+    age,
+  } = req.body;
+  try {
+    const [users] = await pool.query(
+      `UPDATE users SET fullname = ?, username = ?, email = ?, password = ?, role = ?, address = ?, phone_number = ?, age = ? WHERE id = ?`,
+      [
+        fullname,
+        username,
+        email,
+        password,
+        role,
+        address,
+        phone_number,
+        age,
+        id,
+      ]
+    );
+
+    const [userUpdate] = await pool.query(
+      `SELECT id, fullname, username, email, role, address, phone_number, age FROM users WHERE id = ?`,
+      [id]
+    );
+
+    const updateUser = {
+      id: users.insertId,
+      fullname,
+      username,
+      email,
+      role,
+      address,
+      phone_number,
+      age,
+    };
+
+    res.status(200).json({
+      status: "success",
+      message: "User updated successfully",
+      data: userUpdate[0], // password tidak dikembalikan
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const deleteUserHandler = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const [deleteUser] = await pool.query(`DELETE FROM users WHERE id = ?`, [
+      id,
+    ]);
+
+    if (deleteUser.affectedRows === 0) {
+      return res.status(404).json({
+        status: "fail",
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    console.error(error);
   }
 };
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -3,6 +3,8 @@ import {
   addUserHandler,
   getAllUsersHandler,
   getUserByIdHandler,
+  updateUserHandler,
+  deleteUserHandler,
 } from "../handlers/usersHandler.js";
 
 const userRouter = express.Router();
@@ -10,5 +12,7 @@ const userRouter = express.Router();
 userRouter.get("/users", getAllUsersHandler);
 userRouter.get("/users/:id", getUserByIdHandler);
 userRouter.post("/users", addUserHandler);
+userRouter.put("/users/:id", updateUserHandler);
+userRouter.delete("/users/:id", deleteUserHandler);
 
 export default userRouter;
